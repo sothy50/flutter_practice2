cc3e0f6d8fa3978cc5d590dc9209dc73fc6da63c
sothy50 <sstaxyz55ou00@gmail.com>
2022-07-02 08:45:43 +0900

    add input_page.dart

---
+++ lib/input_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
